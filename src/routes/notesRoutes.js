37c378f4574c2d4ff2be0ae9552c29ee319f0e01
João Vitor Sousa <joao.sousa@edu.unirio.br>
2024-05-20 12:34:15 -0300

    Removed old hardcoded notes array

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -2,7 +2,6 @@ import express from "express"
 import NotesController from "../controllers/NotesController.js";
 
 const notesRoutes = express.Router()
-const notes = [{id: 0, title: "Note 1", content: "This is the first note"}, {id: 1, title: "Note 2", content: "This is the second note"}]
 
 notesRoutes.get("/", NotesController.getNotes)
 
