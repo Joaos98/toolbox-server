0d0381be075dd3bfcc1bc4754e544c127f83b2df
João Vitor Sousa <joao.sousa@edu.unirio.br>
2024-05-20 22:33:45 -0300

    Notes CRUD done (i hope)

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -12,6 +12,44 @@ const NotesController = {
         } catch (err) {
             res.status(400).send(err)
         }
+    },
+
+    async createNote(req, res) {
+        try {
+            const newNote = await db.Note.create(req.body)
+            res.status(200).send(newNote.toJSON())
+        } catch (err) {
+            res.status(400).send("There was an error creating a new note!")
+        }
+    },
+
+    async deleteNote(req, res) {
+        try {
+            const note = await db.Note.destroy({
+                where: {
+                    id: req.query.id
+                }
+            })
+            res.status(200).send()
+        } catch (err) {
+            res.status(400).send("There was an error trying to delete your note!")
+        }
+    },
+
+    async updateNotes(req, res) {
+        try {
+            const notes = req.body.notes
+            for (const index in notes) {
+                const noteModel = await db.Note.findByPk(notes[index].id)
+                noteModel.title = notes[index].title
+                noteModel.content = notes[index].content
+                await noteModel.save()
+            }
+            res.status(200).send("All notes were saved!")
+        } catch (err) {
+            res.status(400).send("There was an error trying to save your notes!")
+        }
+
     }
 }
 
--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -4,6 +4,9 @@ import NotesController from "../controllers/NotesController.js";
 const notesRoutes = express.Router()
 
 notesRoutes.get("/", NotesController.getNotes)
+notesRoutes.post("/", NotesController.createNote)
+notesRoutes.delete("/", NotesController.deleteNote)
+notesRoutes.put("/", NotesController.updateNotes)
 
 
 export default notesRoutes
